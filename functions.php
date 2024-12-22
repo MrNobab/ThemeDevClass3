b58cf73c64eb021ea81e99937d3ddcaf30f0d7f0
Mosfikur Rahman Nobab <99020587+MrNobab@users.noreply.github.com>
2024-12-23 01:34:00 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,9 +21,11 @@ if(!function_exists('myThemeFunction')){
     load_theme_textdomain( 'mrTextDomain', get_template_directory() . '/languages' );
 }
 
+
+
 //Add class on li tag
 
-    if(!function_exists('mrthemeliClassAdd')){
+if(!function_exists('mrthemeliClassAdd')){
         function mrthemeliClassAdd($classes, $item, $args){
             if(isset($args->add_li_class)) {
                 $classes[] = $args->add_li_class;
@@ -33,8 +35,10 @@ if(!function_exists('myThemeFunction')){
     }
     add_filter('nav_menu_css_class', 'mrthemeliClassAdd', 1, 3);
 
+
+
 // Add Class on Anchor (a) tag
-    if(!function_exists('add_additional_class_on_a')){
+if(!function_exists('add_additional_class_on_a')){
         function add_additional_class_on_a($classes, $item, $args){
             if (isset($args->add_a_class)) {
                 $classes['class'] = $args->add_a_class;
@@ -45,11 +49,33 @@ if(!function_exists('myThemeFunction')){
 
 }
 
+
+if(!function_exists('mrTheme_custom_post_type')){
+function mrTheme_custom_post_type() {
+	register_post_type('services',
+		array(
+			'labels'      => array(
+				'name'          => __('Services', 'mrTextDomain'),
+				'singular_name' => __('Service', 'mrTextDomain'),
+                'add_new' => __('New service', 'mrTextDomain'),
+                'add_new_item' => __('Add New service', 'mrTextDomain'),
+                'edit_item' => __('Edit service', 'mrTextDomain'),
+                'new_item' => __('New service', 'mrTextDomain'),
+                'not_found' => __('Oops! No Service(s) found !!! ', 'mrTextDomain'),
+			),
+				'public'      => true,
+				'has_archive' => true,
+                'can_export' => false,
+		)
+	);
+}
+}
+add_action('init', 'mrTheme_custom_post_type');
+
+
+
+
 add_action( 'after_setup_theme', 'myThemeFunction');
-
-
-
-
 
 
 
